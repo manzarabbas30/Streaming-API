52c30050e2ba786f50da09dc6001369508a5524c
Manzar <manzarabbasshah1@gmail.com>
2022-12-09 19:09:33 +0500

    Weather API

--- WeatherAPI.php
+++ WeatherAPI.php
@@ -19,6 +19,7 @@ print_r($data);
 // echo "<pre>";
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,25 +29,37 @@ print_r($data);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    div{
+        margin: 3px;
+    }
+    .container {
+        margin: 10px;
+        padding: 20px;
+        border: 1px solid grey;
+        width: fit-content;
+        border-radius: 5px;
+        margin: 0 auto;
+    }
+</style>
 
 <body>
     <hr>
-    <div class="report-container">
-        <h2><?php echo $data->name;?> Weather Status</h2>
+    <div class="container">
+        <h2><?php echo $data->name; ?> Weather Status</h2>
         <div class="time">
             <div>Time : <?php echo date("l g:i a", $currentTime); ?></div>
             <div>Date : <?php echo date("d F, Y", $currentTime); ?></div>
-
             <div><?php echo ucwords($data->weather[0]->description) ?></div>
         </div>
         <div>
             <img src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png" alt="" />
-            <?php echo $data->main->temp_max; ?>&deg;C - 
+            <?php echo $data->main->temp_max; ?>&deg;C -
             <span><?php echo $data->main->temp_min; ?>&deg;C</span>
         </div>
-        <div >
-            <div >Humidty: <?php echo $data->main->humidity;?> %</div>
-            <div >Wind : <?php echo $data->wind->speed;?> km/h</div>
+        <div>
+            <div>Humidty: <?php echo $data->main->humidity; ?> %</div>
+            <div>Wind : <?php echo $data->wind->speed; ?> km/h</div>
         </div>
 
     </div>
